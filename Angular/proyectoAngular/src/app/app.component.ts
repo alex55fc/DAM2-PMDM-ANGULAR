9ceb5b173ea2607811488f0dc088ea689f874522
alexa <alexander.55.fc@gmail.com>
2024-01-08 12:45:36 +0100

    app.component.ts modified to show a message

--- Angular/proyectoAngular/src/app/app.component.ts
+++ Angular/proyectoAngular/src/app/app.component.ts
@@ -1,14 +1,32 @@
+// Importa el decorador 'Component' desde el módulo '@angular/core'
 import { Component } from '@angular/core';
+
+// Importa los módulos 'CommonModule' y 'RouterOutlet' desde '@angular/common' y '@angular/router' respectivamente
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Define un componente con metadatos
 @Component({
+  // Define el selector del componente, este será usado para insertar este componente en otros archivos HTML
   selector: 'app-root',
+  
+  // Propiedad 'standalone' que no es un metadato de un componente Angular, es posible que sea una propiedad personalizada para este proyecto en particular
   standalone: true,
+  
+  // Importa los módulos necesarios para este componente
   imports: [CommonModule, RouterOutlet],
+  
+  template:  '<h1>{{title}}</h1>',
+  /* Antes estaba esto 
+  Define la plantilla HTML que se usará para este componente
   templateUrl: './app.component.html',
+  */
+  // Define la ruta al archivo CSS que se usará para estilos específicos de este componente
   styleUrl: './app.component.css'
 })
+
+// Define la clase del componente
 export class AppComponent {
+  // Define una propiedad 'title' que se puede usar en la plantilla HTML asociada a este componente
   title = 'proyectoAngular';
 }
